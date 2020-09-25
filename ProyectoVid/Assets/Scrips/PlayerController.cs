90f1bb2ef761b343878bdca96aab4cc1e5148004
Panchoapc <fapieper@miuandes.cl>
2020-09-24 21:18:32 -0300

    comienzo de Scripts
    playes movemesnt setup y follow del enemigo

---
+++ ProyectoVid/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(xMove * moveSpeed * Time.deltaTime, yMove * moveSpeed * Time.deltaTime, 0);
+        transform.Translate(move);
+    }
+}
---
+++ ProyectoVid/Assets/Scrips/TrapController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public float zoneOfEffect = 5;
+    private float trapSpeed = 2;
+    Transform trapMovable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trapMovable = GetComponentInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionTrigger2D(Collider2D collider)
+    {
+        if (collider.tag=="Player")
+        {
+            Vector3 moveVector = new Vector3(trapSpeed * Time.deltaTime, 0, 0);
+            for (int i = 0; i < zoneOfEffect*2; i++)
+            {
+                transform.Translate(moveVector*i);
+            }
+        }
+    }
+}
