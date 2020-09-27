d6aac117791aec0078bfd78c9e4f8b0fe30c36b4
Ignacio F. Garcés <ifgarces@miuandes.cl>
2020-09-27 16:45:54 -0300

    Fixed sprites, added player colliders

---
+++ ProyectoVid/Assets/Scrips/Characters/CharacterAux.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CharacterAux : MonoBehaviour
+{
+    /// <summary>
+    /// Voltea el sprite de `obj` según movimiento en eje X `xMovement`.
+    /// </summary>
+    /// <param name="spriteSize">Tamaño en Unity del sprite CUADRADO asociado a `obj`.</param>
+    public static void FlipMovementX(MonoBehaviour obj, float xMovement, float spriteSize) {
+        if (xMovement > 0) {
+            obj.transform.localScale = new Vector3(spriteSize, spriteSize, spriteSize);
+        }
+        else if (xMovement < 0) {
+            obj.transform.localScale = new Vector3(-spriteSize, spriteSize, spriteSize);
+        }
+    }
+}
