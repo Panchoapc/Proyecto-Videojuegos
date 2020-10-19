faf50fb0288f5bad73dda9861cb33a311ecf7c32
Ignacio F. Garcés <ifgarces@miuandes.cl>
2020-10-19 16:40:57 -0300

    Added camera tracking to player

--- ProyectoVid/Assets/Scrips/Characters/Player.cs
+++ ProyectoVid/Assets/Scrips/Characters/Player.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Character {
-    [Range(1,100)] private int mentalSanity = 100; // vida (sanidad mental) ϵ [0, 100]
+public class Player : Character
+{
+    [SerializeField] private Insanity sanityBar;
+    [Range(0,100)] private int mentalSanity; // vida (sanidad mental)
 
     void Start()
     {
         this.moveSpeed = 5;
-        Debug.Log("Player started.");
+        this.mentalSanity = 100;
     }
     
     void Update()
@@ -22,8 +24,26 @@ public class Player : Character {
         float yMove = Input.GetAxisRaw("Vertical");
 
         this.FlipOnMovementX(xMove);
-        //CharacterAux.FlipMovementX(this, xMove, this.spriteSize);
 
-        this.transform.position += new Vector3(xMove, yMove, 0) * Time.deltaTime * this.moveSpeed;
+        this.transform.position += new Vector3(xMove, yMove, 0) * this.moveSpeed * Time.deltaTime;
+    }
+
+    void takeDamage(int dmg)
+    {
+        mentalSanity += dmg;
+        sanityBar.setSanity(mentalSanity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(string.Format("Player collided with {0}", collision.tag));
+
+        /// sería más fácil hacer este trigger en cada enemigo, para que cada tipo de enemigo le haga daño distinto al jugador, en vez de poner 1 tag por cada tipo de enemigo
+        
+        if (collision.tag == "Enemy")
+        {
+            //Debug.Log("damge taken");
+            //takeDamage(10);
+        }
     }
 }
