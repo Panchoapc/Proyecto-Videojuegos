7c66d173c2e54d88ccf49e3a22f515fff21e87f6
Panchoapc <fapieper@miuandes.cl>
2020-10-26 15:09:51 -0300

    Creado el Menu de Muerte con boton de PLAY AGAIN

---
+++ ProyectoVid/Assets/Scrips/EndgameMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndgameMenu : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); // vamos a la escena anterior que corresponde a la del juego
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit(); // este comando no funciona en el editor de UNITY
+    }
+}
---
+++ ProyectoVid/Assets/Scrips/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        lifeCkeck();
+    }
+
+    void lifeCkeck()
+    {
+        int currentLife = FindObjectOfType<Player>().getCurrentLives();
+        if (currentLife <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
