05947ac561a4f8af0c28090fe6e877cd9ecd212c
RaimundoDiaz <53487052+RaimundoDiaz@users.noreply.github.com>
2020-10-26 21:34:34 -0300

    added sign script

---
+++ ProyectoVid/Assets/Scrips/FloorTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class FloorTrigger : MonoBehaviour
+{
+    AudioSource audiosourceCollision;
+    public AudioSource playSound;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter");
+        playSound.Play();
+    }
+}
