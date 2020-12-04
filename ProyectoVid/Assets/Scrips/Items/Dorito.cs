1a6b60cc49ff55adf932a305547038b829a0fc02
RaimundoDiaz <53487052+RaimundoDiaz@users.noreply.github.com>
2020-12-04 20:17:18 -0300

    dorito added

---
+++ ProyectoVid/Assets/Scrips/Items/Dorito.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dorito : MonoBehaviour
+{
+    public readonly int HP = 10;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Player p = FindObjectOfType<Player>();
+            if (p.mentalSanity == Player.MAX_SANITY)
+            {
+                Debug.LogFormat("[Avocado] Player health is full!");
+                return;
+            }
+            p.Heal(HP);
+            Destroy(this.gameObject);
+        }
+    }
+}
