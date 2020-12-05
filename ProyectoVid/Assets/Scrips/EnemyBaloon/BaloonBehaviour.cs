706aa31c8e558bb2b6dd92063f6510c21dd772e7
RaimundoDiaz <53487052+RaimundoDiaz@users.noreply.github.com>
2020-12-05 20:44:47 -0300

    balloon enemy movement added

---
+++ ProyectoVid/Assets/Scrips/EnemyBaloon/BaloonBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaloonBehaviour : MonoBehaviour
+{
+     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float stopPosition = 3f;
+    [SerializeField] float startPosition = 0f;
+    Rigidbody2D myRigidbody;
+    BoxCollider2D myBoxCollider;
+
+    BalooonMovement movement;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
+        myRigidbody.velocity = new Vector2(0f, moveSpeed);
+        movement = GetComponent<BalooonMovement>();
+    }
+
+
+    void Update()
+    {
+        movement.Move(stopPosition, startPosition, moveSpeed, myRigidbody);
+    }
+}
---
+++ ProyectoVid/Assets/Scrips/EnemyBaloon/BalooonMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalooonMovement : MonoBehaviour
+{
+    public void Move(float stopPosition, float startPosition, float moveSpeed, Rigidbody2D myRigidbody)
+    {
+        if (transform.position.y <= (startPosition - stopPosition))
+        {
+            myRigidbody.velocity = new Vector2(0f, moveSpeed);
+        }
+        else if (transform.position.y >= (startPosition + stopPosition))
+        {
+            myRigidbody.velocity = new Vector2(0f, -moveSpeed);
+        }
+    }
+}
