d119a0b9684e526b7ba4ec4f8eecce86b82a2f7a
RaimundoDiaz <53487052+RaimundoDiaz@users.noreply.github.com>
2020-12-06 16:51:16 -0300

    balloon finished

--- ProyectoVid/Assets/Scrips/EnemyBaloon/BaloonBehaviour.cs
+++ ProyectoVid/Assets/Scrips/EnemyBaloon/BaloonBehaviour.cs
@@ -2,27 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaloonBehaviour : MonoBehaviour
+public class BaloonBehaviour : Enemy
 {
-     [SerializeField] float moveSpeed = 1f;
-    [SerializeField] float stopPosition = 3f;
-    [SerializeField] float startPosition = 0f;
+    [SerializeField] float MOVE_SPEED = 1f; // velocidad base de movimiento
+    [SerializeField] float startPosition = 0f; // posicion inicial
+    [SerializeField] float stopPosition = 3f; // radio de movimiento (offset)
+    public static readonly int TOUCH_ATTACK = 20; // ataque por contacto
+    public static readonly int MAX_HEALTH = 150;
     Rigidbody2D myRigidbody;
     BoxCollider2D myBoxCollider;
 
+    private Player player = null;
     BalooonMovement movement;
+    [SerializeField] private Animator animator = null;
+    private bool isInNightmareMode = false;
+    private Factory factory = null;
 
-    void Start()
+    protected override void Start()
     {
+        this.moveSpeed = MOVE_SPEED;
+        this.touchAttack = TOUCH_ATTACK;
+        this.health = MAX_HEALTH;
         myRigidbody = GetComponent<Rigidbody2D>();
         myBoxCollider = GetComponent<BoxCollider2D>();
         myRigidbody.velocity = new Vector2(0f, moveSpeed);
         movement = GetComponent<BalooonMovement>();
+        this.player = FindObjectOfType<Player>();
+        this.factory = FindObjectOfType<Factory>();
     }
 
 
-    void Update()
+    protected override void Update()
     {
         movement.Move(stopPosition, startPosition, moveSpeed, myRigidbody);
+
+        bool nightmareCondition = this.player.mentalSanity < Player.NIGHTMARE_SANITY;
+        if (nightmareCondition && !this.isInNightmareMode) this.EnterNightmareMode();
+        else if (!nightmareCondition && this.isInNightmareMode) this.ExitNightmareMode();
+    }
+
+    protected override void EnterNightmareMode()
+    {
+        Debug.LogFormat("[Balloon] Entered nightmare mode!");
+        this.isInNightmareMode = true;
+        animator.SetBool("inNightmareMode", true);
+        this.touchAttack += 25;
+        this.moveSpeed += 4;
+    }
+
+    protected override void ExitNightmareMode()
+    {
+        Debug.LogFormat("[Balloon] Exited nightmare mode.");
+        this.isInNightmareMode = false;
+        animator.SetBool("inNightmareMode", false);
+        this.touchAttack = TOUCH_ATTACK;
+        this.moveSpeed = MOVE_SPEED;
     }
 }
